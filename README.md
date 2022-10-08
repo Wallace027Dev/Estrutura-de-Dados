75d66cb968a70e9c8cdf365abaf7818567da1941
Wallace027Dev <108556424+Wallace027Dev@users.noreply.github.com>
2022-10-08 10:30:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Estrutura-de-Dados
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-ftpne2)
---
+++ index.js
@@ -0,0 +1,7 @@
+const somosCasados = true;
+const anoNascimento = 1980;
+const media = (7+8+9+10)/4;
+const nome= vanessa;
+let idade = null;
+const terceiroFilho
+const numeroG
